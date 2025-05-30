004a29feb2ed2fc3b8b9310ddce1268cc9aef01f
balaji <balaji@instasafe.com>
2025-05-30 15:15:53 +0530

    Initialization of project

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fetch = (...args)=> import('node-fetch').then(({default: fetch})=> fetch(...args));
+const app = express;
+const port = 3001;
+
+app.set('trust proxy', true);
+ 
+app.get('/', async(req,res)=>{
+    const ip = req.ip;
+    const response = await fetch(`https://ipapi.co/${ip}/json/`);
+    const data = await response.json();
+    res.send(`<h1> Your Ip is Info </h1><pre>${JSON.stringify(data,null,2)}</pre>`);
+});
+
+app.listen(port,()=>{
+    console.log(`Node.js running on http:localhost:${port}`);
+});
